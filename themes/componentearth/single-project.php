50115020858aae11b68c04f04ce7591db17398bf
alexammoser <AlexaD@mmoser.com>
2026-04-29 14:08:25 -0700

    Updates

--- themes/componentearth/single-project.php
+++ themes/componentearth/single-project.php
@@ -83,36 +83,17 @@ $image = $custom_image ? $custom_image[0] : "";
                                     <div class="w-[10rem] h-[2px] bg-primary"></div> Project
                                 </div>           
                                 <h2 class="text-primary-dark max-w-[50.875rem] ~text-[2.5rem]/[5.625rem]  tracking-[-2%] leading-[90%]"><?php the_title(); ?></h2>                                
-                                <div class="font-tertiary font-normal text-primary-dark text-[1rem]"><?php the_content(); ?></div>                                
+                                <div class="font-tertiary font-normal text-primary-dark text-[1rem]"><?php the_content(); ?></div>                                           
+                                
+                                <div class="[&_p]:~text-[4rem]/[4.5rem] [&_p]:leading-[90%] [&_p]:tracking-[-2%] [&_p]:text-primary-dark [&_strong]:font-normal [&_strong]:p-[0.25rem] [&_strong]:bg-[var(--theme-color-highlight)]">
+                                    <p>"Quote from the project and how this <strong>impacted the community & ecosystem"</strong></p>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
                 </div>                             
             </div>
-
-            <!-- <div class="flex ~mt-[2rem]/[2.5rem] items-center ~mb-[2rem]/[3rem]">
-                <div class="flex border-y border-solid border-[#F4EDE9]">
-                    <span class="font-primary text-[0.875rem] tracking-[-0.01rem] leading-[140%] text-black ~p-[1.5rem]/[1.75rem] border-l border-r border-solid border-[#F4EDE9]">Share</span>                    
-                    
-                    <a class="group ~p-[1.5rem]/[1.75rem] border-r border-solid border-[#F4EDE9]" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank" rel="nofollow noreferrer noopener">
-                        <svg class="w-[1.5rem] h-[1.5rem]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                            <path class="group-hover:fill-[#C50]" fill-rule="evenodd" clip-rule="evenodd" d="M0 5C0 2.23858 2.23858 0 5 0H27C29.7614 0 32 2.23858 32 5V27C32 29.7614 29.7614 32 27 32H5C2.23858 32 0 29.7614 0 27V5ZM8.2 13.3V24H11.6V13.3H8.2ZM8 9.9C8 11 8.8 11.8 9.9 11.8C11 11.8 11.8 11 11.8 9.9C11.8 8.8 11 8 9.9 8C8.9 8 8 8.8 8 9.9ZM20.6 24H23.8V17.4C23.8 14.1 21.8 13 19.9 13C18.2 13 17 14.1 16.7 14.8V13.3H13.5V24H16.9V18.3C16.9 16.8 17.9 16 18.9 16C19.9 16 20.6 16.5 20.6 18.2V24Z" fill="black"/>
-                        </svg>
-                    </a>
-
-                    <a class="group ~p-[1.5rem]/[1.75rem] border-r border-solid border-[#F4EDE9]" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank" rel="nofollow noreferrer noopener">
-                        <svg class="w-[1.5rem] h-[1.5rem]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                            <path class="group-hover:fill-[#C50]" d="M28 0C30.2091 0 32 1.79086 32 4V28C32 30.2091 30.2091 32 28 32H4C1.79086 32 0 30.2091 0 28V4C0 1.79086 1.79086 0 4 0H28ZM18.0449 7C17.5911 7 16.0128 7.0889 14.7666 8.23535C13.386 9.50578 13.5782 11.0273 13.624 11.291V13.7266H11.3223C11.1443 13.7267 11.0001 13.8709 11 14.0488V17.3291C11 17.5072 11.1442 17.6522 11.3223 17.6523H13.5146V26.6777C13.5148 26.8558 13.6598 27 13.8379 27H17.5547C17.7328 27 17.8768 26.8558 17.877 26.6777V17.6943H20.3975C20.5612 17.6943 20.6989 17.5717 20.7178 17.4092L21.1006 14.0859C21.111 13.9948 21.0824 13.9035 21.0215 13.835C20.9603 13.7663 20.8722 13.7266 20.7803 13.7266H17.877V11.6436C17.877 11.0159 18.2153 10.6974 18.8818 10.6973H20.7803C20.9584 10.6973 21.1025 10.5531 21.1025 10.375V7.3252C21.1025 7.147 20.9585 7.00195 20.7803 7.00195H18.1641C18.1454 7.00105 18.1047 7 18.0449 7Z" fill="black"/>
-                        </svg>
-                    </a>
-
-                    <a class="group ~p-[1.5rem]/[1.75rem] border-r border-solid border-[#F4EDE9]" href="http://twitter.com/intent/tweet?url=<?php echo get_permalink(); ?>" target="_blank" rel="nofollow noreferrer noopener">
-                        <svg class="w-[1.5rem] h-[1.5rem]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                            <path class="group-hover:fill-[#C50]" d="M28 0C30.2091 0 32 1.79086 32 4V28C32 30.2091 30.2091 32 28 32H4C1.79086 32 0 30.2091 0 28V4C0 1.79086 1.79086 0 4 0H28ZM7 8L13.8047 16.751L7.35547 24H10.1172L15.0947 18.4062L19.4453 24H25L17.9062 14.7773L23.9365 8H21.1758L16.6279 13.1123L12.6953 8H7ZM11.8643 9.53906L21.7363 22.375H20.207L10.2236 9.53906H11.8643Z" fill="black"/>
-                        </svg>
-                    </a>
-                </div>
-            </div> -->
         </div>
     </div>
         
