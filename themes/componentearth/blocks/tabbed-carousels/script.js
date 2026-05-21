538c1b50cd44b3fbdcca8d3250142554046cb899
alexammoser <AlexaD@mmoser.com>
2026-05-20 21:37:45 -0700

    Updates

---
+++ themes/componentearth/blocks/tabbed-carousels/script.js
@@ -0,0 +1,34 @@
+baunfire.addModule({
+    init(baunfire) {
+        const $ = baunfire.$;
+
+        const script = () => {
+            const els = $("section.tabbed-carousels");
+            if (!els.length) return;
+
+            els.each(function () {
+                const self = $(this);
+                /* Add your logic here */               
+
+                
+                function switchTab(tabId) {
+                    // Hide all panes and deactivate all buttons
+
+                    $('.tab-pane').removeClass('active');
+                    $('.tab-btn').removeClass('active');
+                    
+                    // Show the selected pane and activate the clicked button
+                    $('.tab-btn[data-tab=' + tabId + ']').addClass('active');    
+                    $('.tab-pane[data-tab=' + tabId + ']').addClass('active');
+                }
+
+                $('.tab-btn').click(function(e) {
+                    switchTab($(this).data('tab'));
+                })
+
+            });
+        }
+
+        script();
+    }
+});
